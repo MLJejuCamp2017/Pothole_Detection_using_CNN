6f2918ca3ca6aacc96dbc6a075ab876cbf8125c2
HyunWoo <chzhqk1994@gmail.com>
2017-07-11 22:04:10 +0900

    Before using window

--- Porthole_Detection/Image_Recognition.py
+++ Porthole_Detection/Image_Recognition.py
@@ -24,6 +24,20 @@ with tf.Session() as sess:
     # Feed the image_data as input to the graph and get first prediction
     softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
+
+
+    # 여기다가 윈도우 슬라이드 코드를 넣고 슬라이드로 잘라낸 이미지를  아래 prediction 에 넣은 후 포트홀 일 경우에만 출력하는 방식으로 구현해보자
+    # image_data 에 이미지 파일의 직접적인 경로 대신 슬라이드로 잘라낸 이미지를 반복적으로 넣는 방식으로 해결 가능할 것 같다.
+    # 슬라이드가 주행기록을 모두 훑을 때 까지 prediction은 반복되어야 한다.
+    # 시간 오래걸리겠다
+
+    # 현재 문제점
+    # 1. 이 파일에 윈도우를 어떻게 구현 할 것인가?
+    # 2. GPS 데이터를 어떻게 기록하고 포트홀일 경우 어떻게 출력하거나 지도에 표시할 것 인가? 이미지파일에 모두 담기엔 조금 그런데;;
+    # 3. SensorStream IMU+GPS 어플리케이션을 현재 안드로이드 버전에서 GPS가 작동하게 만들어야 한다. (마그네틱 센서와 GPS 센서 문제)
+    # 4. 3번 문제를 해결하기 위해 일단 어플리케이션 코드를 분석해야하는데 김도현 교수님께 한번 여쭤보자
+
+
     predictions = sess.run(softmax_tensor, \
                            {'DecodeJpeg/contents:0': image_data})
 
@@ -34,3 +48,6 @@ with tf.Session() as sess:
         human_string = label_lines[node_id]
         score = predictions[0][node_id]
         print('%s (score = %.5f)' % (human_string, score))
+
+        if label_lines[0] == "speedbump":
+            print("gg")
